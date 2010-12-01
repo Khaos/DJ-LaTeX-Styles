fb0aca41a9db6607efd7f945a67e98311a8290fa
Dazhi Jiang <jiangdzh@gmail.com>
2010-12-01 03:09:34 +0000

    Fixed the problem in Issue 2. A new \newif cannot be defined in another \if, that is, all IFs have to be pre-defined before they are used. Adjust the page width to 30pc.

--- Test/DJThesis/stypull.sh
+++ Test/DJThesis/stypull.sh
@@ -1,1 +1,5 @@
 cp ../../Source/DJThesis/PhDThesis01/SUACSEThesis.sty SUACSEThesis.sty
+
+# pdflatex -shell-escape -interaction=nonstopmode -file-line-error SUACSEThesis-test01.tex
+
+# open -a Skim.app SUACSEThesis-test01.pdf
