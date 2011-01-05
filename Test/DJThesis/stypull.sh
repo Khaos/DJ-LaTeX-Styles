c48b304e20ee7b329dade4530e961bf4f62a1f81
Dazhi Jiang <jiangdzh@gmail.com>
2011-01-05 00:03:07 +0000

    Recommit, define the new title page style

--- Test/DJThesis/stypull.sh
+++ Test/DJThesis/stypull.sh
@@ -2,6 +2,9 @@
 cp ../../Source/DJThesis/PhDThesis01/SUACSEThesis.sty SUACSEThesis.sty
 cp ../../Source/DJThesis/PhDThesis01/SUACSEThesisMath.sty SUACSEThesisMath.sty
 
+cp ../../Source/DJThesis/PhDThesis01/SUACSEThesis.sty tmp/SUACSEThesis.sty
+cp ../../Source/DJThesis/PhDThesis01/SUACSEThesisMath.sty tmp/SUACSEThesisMath.sty
+
 # pdflatex -shell-escape -interaction=nonstopmode -file-line-error SUACSEThesis-test01.tex
 
 # open -a Skim.app SUACSEThesis-test01.pdf
